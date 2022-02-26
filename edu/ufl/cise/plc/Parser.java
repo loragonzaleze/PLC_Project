297a391f2074f72485da265dbd2bbbb265e419bf
loragonzaleze <61980534+loragonzaleze@users.noreply.github.com>
2022-02-26 18:34:03 -0500

    Assignment 3 we both worked on same computer

--- edu/ufl/cise/plc/Parser.java
+++ edu/ufl/cise/plc/Parser.java
@@ -2,7 +2,9 @@ package edu.ufl.cise.plc;
 
 import edu.ufl.cise.plc.ast.*;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
 
 public class Parser implements IParser {
 
@@ -56,6 +58,161 @@ public class Parser implements IParser {
         return lexer.peek().getKind() == token;
     }
 
+    private Program program() throws PLCException {
+        IToken firstToken = current;
+        Types.Type returnType = Types.Type.toType(current.getText());
+
+        consume();
+        match(IToken.Kind.IDENT);
+        String programName = current.getText();
+        consume();
+        match(IToken.Kind.LPAREN);
+        consume();
+        List<NameDef> parameters = paramList();
+        match(IToken.Kind.RPAREN);
+        consume();
+        List<ASTNode> decsAndStatements = decsAndStatementsList();
+
+        return new Program(firstToken, returnType, programName, parameters, decsAndStatements);
+    }
+
+    private List<NameDef> paramList() throws PLCException {
+        List<NameDef> params = new ArrayList<>();
+        while(current.getKind() == IToken.Kind.TYPE || current.getKind() == IToken.Kind.KW_CONSOLE){
+
+            IToken firstToken = current;
+            String type = current.getText();
+            consume();
+
+            Dimension dimension = dimension();
+
+            match(IToken.Kind.IDENT);
+            String name = current.getText();
+            consume();
+            if(dimension == null)
+                params.add(new NameDef(firstToken, type, name));
+            else
+                params.add(new NameDefWithDim(firstToken, type, name, dimension));
+
+            if(current.getKind() != IToken.Kind.COMMA)
+                break;
+
+            match(IToken.Kind.COMMA);
+            consume();
+        }
+        if(current.getKind() == IToken.Kind.KW_VOID)
+            throw new SyntaxException("Cannot have void type");
+        return params;
+    }
+
+    private Dimension dimension() throws PLCException {
+        IToken firstToken = current;
+        if(firstToken.getKind() != IToken.Kind.LSQUARE)
+            return null;
+
+        match(IToken.Kind.LSQUARE);
+        consume();
+        Expr width = expr();
+        match(IToken.Kind.COMMA);
+        consume();
+        Expr height = expr();
+        match(IToken.Kind.RSQUARE);
+        consume();
+
+        return new Dimension(firstToken, width, height);
+    }
+    private NameDef nameDef() throws PLCException {
+        IToken firstToken = current;
+        String type = current.getText();
+        consume();
+
+        Dimension dimension = dimension();
+
+        match(IToken.Kind.IDENT);
+        String name = current.getText();
+        consume();
+        if(dimension == null)
+            return new NameDef(firstToken, type, name);
+        else
+            return new NameDefWithDim(firstToken, type, name, dimension);
+    }
+
+    private List<ASTNode> decsAndStatementsList() throws PLCException {
+        List<ASTNode> decsAndStatements = new ArrayList<>();
+        while(current.getKind() != IToken.Kind.EOF){
+            if(current.getKind() == IToken.Kind.TYPE || current.getKind() == IToken.Kind.KW_CONSOLE){ // Declaration
+                ASTNode declaration = declaration();
+                decsAndStatements.add(declaration);
+            }
+            else if(current.getKind() == IToken.Kind.IDENT || current.getKind() == IToken.Kind.KW_WRITE || current.getKind() == IToken.Kind.RETURN){ // Statement
+                ASTNode statement = statement();
+                decsAndStatements.add(statement);
+            }
+            match(IToken.Kind.SEMI);
+            consume();
+        }
+        return decsAndStatements;
+    }
+
+    private ASTNode declaration() throws PLCException {
+        IToken firstToken = current;
+        NameDef nameDef = nameDef();
+        if(current.getKind() == IToken.Kind.ASSIGN || current.getKind() == IToken.Kind.LARROW) {
+            IToken op = current;
+            consume();
+            Expr expr = expr();
+            return new VarDeclaration(firstToken, nameDef, op, expr);
+        }
+
+        return new VarDeclaration(firstToken, nameDef, null, null);
+    }
+
+    private ASTNode statement() throws PLCException {
+        IToken firstToken = current;
+
+        if(current.getKind() == IToken.Kind.IDENT){
+            String name = firstToken.getText();
+
+            match(IToken.Kind.IDENT);
+            consume();
+            PixelSelector optionalPS = pixelSelector();
+            if(current.getKind() == IToken.Kind.ASSIGN){ // IDENT PixelSelector? '=' Expr
+                match(IToken.Kind.ASSIGN);
+                consume();
+                Expr expr = expr();
+                return new AssignmentStatement(firstToken, name, optionalPS, expr);
+
+            }
+            else if(current.getKind() == IToken.Kind.LARROW){ //IDENT PixelSelector? ‘<-’ Expr
+                match(IToken.Kind.LARROW);
+                consume();
+                Expr expr = expr();
+                return new ReadStatement(firstToken, name, optionalPS, expr);
+            }
+            throw new SyntaxException("Expected '=' or '<-', got: " + current.getKind());
+        }
+        else if(current.getKind() == IToken.Kind.KW_WRITE){
+            match(IToken.Kind.KW_WRITE);
+            consume();
+
+            Expr source = expr();
+            match(IToken.Kind.RARROW);
+            consume();
+            Expr dest = expr();
+
+            return new WriteStatement(firstToken, source, dest);
+        }
+
+        else if(current.getKind() == IToken.Kind.RETURN){
+            match(IToken.Kind.RETURN);
+            consume();
+            Expr returnExpr = expr();
+
+            return new ReturnStatement(firstToken, returnExpr);
+        }
+        throw new SyntaxException("Expected IDENT, write, or ^, got: " + current.getKind());
+    }
+
     //Expr::= ConditionalExpr | LogicalOrExpr
     private Expr expr() throws PLCException {
         IToken firstToken = current;
@@ -64,11 +221,12 @@ public class Parser implements IParser {
             e = conditionalExpr();
         }
         else if(logicalOrPredictSet.contains(firstToken.getKind()) || firstToken.getKind() == IToken.Kind.BANG || firstToken.getKind() == IToken.Kind.MINUS //PREDICT(Expr ::= LogicalOrExpr) = {!,-,COLOR_OP,IMAGE_OP)
-               || firstToken.getKind() == IToken.Kind.COLOR_OP || firstToken.getKind() == IToken.Kind.IMAGE_OP || firstToken.getKind() == IToken.Kind.IDENT){
+               || firstToken.getKind() == IToken.Kind.COLOR_OP || firstToken.getKind() == IToken.Kind.IMAGE_OP || firstToken.getKind() == IToken.Kind.IDENT || firstToken.getKind() == IToken.Kind.LANGLE
+                || firstToken.getKind() == IToken.Kind.COLOR_CONST || firstToken.getKind() == IToken.Kind.KW_CONSOLE){
             e = logicalOrExpr();
         }
         else{
-            throw new PLCException("Throwing error due to invalid token: " + firstToken.getText() + " token kind: " + firstToken.getKind());
+            throw new SyntaxException("Throwing error due to invalid token: " + firstToken.getText() + " token kind: " + firstToken.getKind());
         }
 
         return e;
@@ -197,6 +355,9 @@ public class Parser implements IParser {
         if(e == null){ // For unaryExprPostfix
             Expr left = primaryExpr();
             PixelSelector right = null;
+            if(current.getKind() == IToken.Kind.SEMI){
+                return left;
+            }
             consume();
             right = pixelSelector();
             if(right != null) //If it does not contain a pixel selector
@@ -222,13 +383,35 @@ public class Parser implements IParser {
             case INT_LIT -> new IntLitExpr(firstToken);
             case FLOAT_LIT -> new FloatLitExpr(firstToken);
             case IDENT -> new IdentExpr(firstToken);
+            case COLOR_CONST -> new ColorConstExpr(firstToken);
+            case KW_CONSOLE -> new ConsoleExpr(firstToken);
             default -> null;
         };
         if(e == null){
-            match(IToken.Kind.LPAREN); // Has to be left parentheses, or else error
-            consume();
-            e = expr();
-            match(IToken.Kind.RPAREN);
+            if(current.getKind() == IToken.Kind.LANGLE){
+                match(IToken.Kind.LANGLE);
+                consume();
+                Expr red = expr();
+                match(IToken.Kind.COMMA);
+                consume();
+                Expr green = expr();
+                match(IToken.Kind.COMMA);
+                consume();
+                Expr blue = expr();
+                match(IToken.Kind.RANGLE);
+                consume();
+                e = new ColorExpr(firstToken, red, green, blue);
+
+            }
+            else if(current.getKind() == IToken.Kind.LPAREN){
+                match(IToken.Kind.LPAREN); // Has to be left parentheses, or else error
+                consume();
+                e = expr();
+                match(IToken.Kind.RPAREN);
+            }
+            else{
+                throw new SyntaxException("Expected Boolean, String, Int, Float, Ident, !, -, COLOR_OP, IMAGE_OP, (,  <<, Actual token: " + firstToken.getKind());
+            }
         }
 
         return e;
@@ -248,6 +431,6 @@ public class Parser implements IParser {
     }
     @Override
     public ASTNode parse() throws PLCException {
-        return expr();
+        return program();
     }
 }
--- edu/ufl/cise/plc/ast/BooleanLitExpr.java
+++ edu/ufl/cise/plc/ast/BooleanLitExpr.java
@@ -22,4 +22,8 @@ public class BooleanLitExpr extends Expr  {
 		return "BooleanLitExpr [getValue()=" + getValue() + "]";
 	}
 
+
+	
+	
+	
 }
